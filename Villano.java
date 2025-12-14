defe56f34ef7e2ddf5022f3cd42a3561f237795b
Dark-riddle <bran.smith.101089@hotmail.com>
2025-12-14 14:32:35 -0500

    Update Villano.java

--- Villano.java
+++ Villano.java
@@ -4,8 +4,9 @@ super(nombre, poder, vida);
 }
 @Override
 public void atacar(Personaje enemigo) {
-System.out.println(nombre + " lanza un Rayo de la Muerte ");
+System.out.println(nombre + "lanza un Rayo de la Muerte ");
 enemigo.vida -= poder * 1.5; 
 if (enemigo.vida < 0) enemigo.vida = 0;
 }
+
 }
